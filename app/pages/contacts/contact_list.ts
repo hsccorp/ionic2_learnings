3f7e18835cb563291f5e7a69dea9d9e26dc84392
Hemant Bansal <hemant.bansal@hsc.com>
2016-09-08 16:03:52 +0530

    contact list item aligned

--- app/pages/contacts/contact_list.ts
+++ app/pages/contacts/contact_list.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {ContactView} from '../../components/my-component/contact-view';
 import {Contacts} from 'ionic-native';
+import { LoadingController } from 'ionic-angular';
 
 @Component({
   templateUrl: 'build/pages/contacts/contact_list.html',
@@ -9,23 +10,29 @@ import {Contacts} from 'ionic-native';
 })
 export class ContactList {
 public contactsfound:any;
- constructor(public navCtrl: NavController) {
+public loader:any;
+ constructor(public navCtrl: NavController,public loadingCtrl: LoadingController) {
+ this.presentLoading();
  this.contactsfound = [];
+
  
  //Hemant: Need to explore more about the way to get the contact list
  // as it is taking 7-10 second to load
  
- Contacts.find(['displayName1']).then((contacts) => {
+ Contacts.find(['*'], {filter: ""}).then((contacts) => {
+  this.loader.dismiss();
           this.contactsfound = contacts;
           //alert(contacts.length);
           alert(JSON.stringify(contacts[0]));
           })
           
           
-          
+  }
   
-
-    
-
+  presentLoading() {
+   this.loader = this.loadingCtrl.create({
+      content: "Loading contacts...",
+    });
+    this.loader.present();
   }
 }
